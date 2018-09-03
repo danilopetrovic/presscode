a40802affd3e089f27b9a7913e1535cdc0518af9
danilopetrovic <danilo86petrovic@gmail.com>
2018-09-04 00:07:56 +0200

    menjano dosta oko footera

--- primer2/footer.php
+++ primer2/footer.php
@@ -3,6 +3,8 @@
 ============================-->
 <style>
     #kakoDoNas {
+        width: 260px;
+        text-align: center;
         position: absolute;
         background-color: #111;
         padding: 0 30px 5px 30px;
@@ -10,32 +12,47 @@
         font-weight: bold;
         font-size: 1.1rem;
         top: 0;
-        left: 40%;
-        animation: kakoDoNasAnimacija 1.5s alternate infinite;
+        left: calc(50% - 130px);
+        /*left: 40%;*/
+        /*left: 30vw;*/
+        animation: kakoDoNasAnimacija 1.5s alternate infinite linear;
     }
     @keyframes kakoDoNasAnimacija {
         from {
             padding: 0 30px 5px 30px;
         }
         to {
-            padding: 2px 25px 5px 25px;
+            padding: 8px 25px 5px 25px;
             font-size: 1.2rem;
         }
+    }
+    #textLinkZa3d {
+        width: 300px;
+        text-align: center;
+        position: absolute;
+        padding: 0 30px 5px 30px;
+        border-radius: 0 0 30% 30%;
+        font-weight: bold;
+        font-size: 1.1rem;
+        bottom: 10px;
+        line-height: 30px;
+        left: calc(50% - 150px);
+        text-shadow: 2px 2px 10px black;
     }
 </style>
 
 <footer id="footer">
     <div class="footer-top">
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-6" id="iznad">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2828.3651831268553!2d20.36855231526132!3d44.85485997909845!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDTCsDUxJzE3LjUiTiAyMMKwMjInMTQuNyJF!5e0!3m2!1sen!2srs!4v1531304298563" width="100%" height="250" frameborder="0" style="border:0" allowfullscreen></iframe>
-                <a id="kakoDoNas" href=https://maps.google.com?saddr=Current+Location&daddr=Press+Code target="_blank">Kako do nas?</a>
+                <a id="kakoDoNas" href=https://maps.google.com?saddr=Current+Location&daddr=Press+Code target="_blank">Klik za put do nas!</a>
             </div>
 
             <div class="col-md-6" id="mapa3d">
-                <a href="https://www.google.com/maps/@44.8548128,20.3705338,0a,82.2y,110.73h,88.52t/data=!3m4!1e1!3m2!1sL0v1EaULVmo2a1wV_ftUxQ!2e0?source=apiv3" target="_blank"><img src="img/mapa/3d-view.jpg" alt="3d-view-Slika" width="100%" height="250"></a>
-
+                <div><a href="https://www.google.com/maps/@44.8548128,20.3705338,0a,82.2y,110.73h,88.52t/data=!3m4!1e1!3m2!1sL0v1EaULVmo2a1wV_ftUxQ!2e0?source=apiv3" target="_blank"><img src="img/mapa/3d-view2.jpg" alt="3d-view-Slika" width="100%" height="250"></a></div>
                 <!--style="pointer-events:none;"-->
+                <a href="https://www.google.com/maps/@44.8548128,20.3705338,0a,82.2y,110.73h,88.52t/data=!3m4!1e1!3m2!1sL0v1EaULVmo2a1wV_ftUxQ!2e0?source=apiv3" id="textLinkZa3d" target="_blank"><h2><strong>Press Code<strong></h2><br>Klik za google 3D ulice!</a>
             </div>
         </div>
     </div>
