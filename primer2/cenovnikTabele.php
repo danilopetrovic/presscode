ef188f9edabe9ed3e2b89a4035312ab53e1e80e0
Danilo <danilo86petrovic@gmail.com>
2018-09-09 19:07:28 +0200

    odvojen cenovnik u 3 fajla, cenovnik.php, cenovnikTabele.php, i kalkulator.js! sredjen kalkulator po tabeli cenovnika koji su mi dali...

---
+++ primer2/cenovnikTabele.php
@@ -0,0 +1,197 @@
+
+                    <div class="row">
+                        <div class="col-12">
+                            <table id="legendaTabela" class="wow fadeInUp">
+                                <tr>
+                                    <td style="background: linear-gradient(to right, rgb(0, 255, 255), rgb(255, 0, 255), rgb(255, 255, 0), rgb(0, 0, 0))">
+                                        &nbsp;&nbsp;&nbsp;
+                                    </td>
+                                    <td>4 - kolor</td>
+                                </tr>
+                                <tr>
+                                    <td style="background-color: black">&nbsp;&nbsp;&nbsp;</td>
+                                    <td>1 - crna</td>
+                                </tr>
+                                <tr>
+                                    <td style="background-color: white">&nbsp;&nbsp;&nbsp;</td>
+                                    <td>0 - bez boje</td>
+                                </tr>
+                            </table>
+                            <p>Vizit karte 90×50mm + plastifikacija (sjaj/mat):</p>
+                            <table class="wow fadeInUp">
+                                <thead>
+                                <tr>
+                                    <th>&nbsp;</th>
+                                    <th colspan="8"><strong>Tiraž</strong></th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <th style="width: 20%;">Print</th>
+                                    <th>60</th>
+                                    <th>100</th>
+                                    <th>200</th>
+                                    <th>300</th>
+                                    <th>400</th>
+                                    <th>500</th>
+                                    <th>1000</th>
+                                    <th>2000</th>
+                                </tr>
+                                <tr>
+                                    <th>4/0</th>
+                                    <td>380</td>
+                                    <td>540</td>
+                                    <td>720</td>
+                                    <td>820</td>
+                                    <td>900</td>
+                                    <td>1000</td>
+                                    <td>1800</td>
+                                    <td>2700</td>
+                                </tr>
+                                <tr>
+                                    <th>4/1</th>
+                                    <td>530</td>
+                                    <td>750</td>
+                                    <td>980</td>
+                                    <td>1200</td>
+                                    <td>1330</td>
+                                    <td>1480</td>
+                                    <td>2430</td>
+                                    <td>4500</td>
+                                </tr>
+                                <tr>
+                                    <th>4/4</th>
+                                    <td>650</td>
+                                    <td>880</td>
+                                    <td>1200</td>
+                                    <td>1350</td>
+                                    <td>1500</td>
+                                    <td>1800</td>
+                                    <td>3050</td>
+                                    <td>5250</td>
+                                </tr>
+                                <tr>
+                                    <th>1/0</th>
+                                    <td>275</td>
+                                    <td>385</td>
+                                    <td>600</td>
+                                    <td>700</td>
+                                    <td>820</td>
+                                    <td>930</td>
+                                    <td>1400</td>
+                                    <td>2300</td>
+                                </tr>
+                                <tr>
+                                    <th>1/1</th>
+                                    <td>340</td>
+                                    <td>530</td>
+                                    <td>800</td>
+                                    <td>1040</td>
+                                    <td>1150</td>
+                                    <td>1360</td>
+                                    <td>2080</td>
+                                    <td>4150</td>
+                                </tr>
+                                </tbody>
+                            </table>
+
+                            <p>Digitalni print na papirima od 80g do 350g:</p>
+                            <table class="wow fadeInUp">
+                                <thead>
+                                <tr>
+                                    <th>&nbsp;</th>
+                                    <th colspan="6">Cena po tabaku</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <th style="width: 20%;">Print</th>
+                                    <th>1-10</th>
+                                    <th>11-50</th>
+                                    <th>51-100</th>
+                                    <th>101-250</th>
+                                    <th>251-500</th>
+                                    <th>&gt;500</th>
+                                </tr>
+                                <tr>
+                                    <th>4/0</th>
+                                    <td>80</td>
+                                    <td>70</td>
+                                    <td>55</td>
+                                    <td>47</td>
+                                    <td>36</td>
+                                    <td>27</td>
+                                </tr>
+                                <tr>
+                                    <th>4/1</th>
+                                    <td>98</td>
+                                    <td>88</td>
+                                    <td>73</td>
+                                    <td>61</td>
+                                    <td>51</td>
+                                    <td>37</td>
+                                </tr>
+                                <tr>
+                                    <th>4/4</th>
+                                    <td>116</td>
+                                    <td>106</td>
+                                    <td>86</td>
+                                    <td>77</td>
+                                    <td>60</td>
+                                    <td>42</td>
+                                </tr>
+                                <tr>
+                                    <th>1/0</th>
+                                    <td>23</td>
+                                    <td>22</td>
+                                    <td>21</td>
+                                    <td>19</td>
+                                    <td>16</td>
+                                    <td>13</td>
+                                </tr>
+                                <tr>
+                                    <th>1/1</th>
+                                    <td>32</td>
+                                    <td>30</td>
+                                    <td>28</td>
+                                    <td>26</td>
+                                    <td>23</td>
+                                    <td>17</td>
+                                </tr>
+                                </tbody>
+                                <tfoot>
+                                <tr>
+                                    <td colspan="7"><strong>Dimenzije Tabaka:</strong> SRA3+(320x488mm)</td>
+                                </tr>
+                                </tfoot>
+                            </table>
+
+                            <p>Plastifikacija:</p>
+                            <table class="wow fadeInUp">
+                                <tr>
+                                    <th colspan="2">Vrsta plastifikacije</th>
+                                    <th>Cena po tabaku</th>
+                                </tr>
+                                <tr>
+                                    <th>mat</th>
+                                    <th>jednostrano</th>
+                                    <td>8 din</td>
+                                </tr>
+                                <tr>
+                                    <th>mat</th>
+                                    <th>dvostrano</th>
+                                    <td>16 din</td>
+                                </tr>
+                                <tr>
+                                    <th>sjaj</th>
+                                    <th>jednostrano</th>
+                                    <td>5 din</td>
+                                </tr>
+                                <tr>
+                                    <th>sjaj</th>
+                                    <th>dvostrano</th>
+                                    <td>10 din</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
--- primer2/kalkulator.js
+++ primer2/kalkulator.js
@@ -1,6 +1,14 @@
 const tiraz = document.querySelector('#tiraz');
 const padajuci = document.getElementsByTagName('select');
 const prikaziCenu = document.getElementById('prikaziCenu');
+const cenovnik = {
+    /*1, 11, 51, 101, 251, 501*/
+    kolor40: [80, 70, 55, 47, 36, 27],
+    kolor41: [98, 88, 73, 61, 51, 37],
+    kolor44: [116, 106, 86, 77, 60, 42],
+    crno10: [23, 22, 21, 19, 16, 13],
+    crno11: [32, 30, 28, 26, 23, 17]
+};
 
 /*~~~resetovanje svih polja na nulu~~~*/
 // tiraz.value = 1;
@@ -22,24 +30,6 @@ function promenaTexta(e) {
 }
 /*~~~bigovanje promena texta~~~KRAJ*/
 
-function izracunajCenu() {
-    let rezultat = 0;
-    for (let i = 0; i < padajuci.length; i++) {
-        rezultat += parseInt(padajuci[i].value);
-    }
-    if (!isNaN(rezultat) && rezultat > 0 && padajuci[0].value !== "0" && padajuci[1].value !== "0") {
-        prikaziCenu.value = rezultat * tiraz.value;
-        prikaziCenu.style.color = '#495057';
-        prikaziCenu.style.fontWeight = 'bold';
-    }
-    else {
-        prikaziCenu.value = 'Odaberite jos opcija kako bi proracun bio tacan...';
-        prikaziCenu.style.fontWeight = 'normal';
-        prikaziCenu.style.color = 'red';
-    }
-    // prikaziCenu.style.border = "1px solid #ced4da";
-    // console.log(rezultat);
-}
 
 /*~~~dodavanje event listenera na sva polja~~~*/
 tiraz.addEventListener('input', function () {
@@ -54,3 +44,98 @@ for (let i = 0; i < padajuci.length; i++) {
     });
 }
 /*~~~dodavanje event listenera na sva polja~~~KRAJ*/
+
+// function izracunajCenu() {
+//     let rezultat = 0;
+//     for (let i = 0; i < padajuci.length; i++) {
+//         rezultat += parseFloat(padajuci[i].value);
+//     }
+//     if (!isNaN(rezultat) && rezultat > 0 && padajuci[0].value !== "0" && padajuci[1].value !== "0") {
+//         prikaziCenu.value = rezultat * tiraz.value;
+//         prikaziCenu.style.color = '#495057';
+//         prikaziCenu.style.fontWeight = 'bold';
+//     }
+//     else {
+//         prikaziCenu.value = 'Odaberite jos opcija kako bi proracun bio tacan...';
+//         prikaziCenu.style.fontWeight = 'normal';
+//         prikaziCenu.style.color = 'red';
+//     }
+//     // prikaziCenu.style.border = "1px solid #ced4da";
+//     // console.log(rezultat);
+// }
+
+function izracunajCenu() {
+    let rezultat = 0;
+    let rezultatCenovnik = 0;
+    if (padajuci[0].selectedIndex > 0 && padajuci[0].selectedIndex <= 5) {
+        // console.log('pripada opciji za matricu cenovnika')
+        for (let i in cenovnik) {
+            if (i === padajuci[0].value) {
+                console.log(i + ' - jeste');
+                console.log(cenovnik[i]);
+
+                switch (true) {
+                    case tiraz.value > 10 && tiraz.value <=50:
+                        rezultatCenovnik = cenovnik[i][1];
+                        console.log(rezultatCenovnik);
+                        break;
+                    case tiraz.value > 50 && tiraz.value <=100:
+                        rezultatCenovnik = cenovnik[i][2];
+                        console.log(rezultatCenovnik);
+                        break;
+                    case tiraz.value > 100 && tiraz.value <=250:
+                        rezultatCenovnik = cenovnik[i][3];
+                        console.log(rezultatCenovnik);
+                        break;
+                    case tiraz.value > 250 && tiraz.value <=500:
+                        rezultatCenovnik = cenovnik[i][4];
+                        console.log(rezultatCenovnik);
+                        break;
+                    case tiraz.value > 500:
+                        rezultatCenovnik = cenovnik[i][5];
+                        console.log(rezultatCenovnik);
+                        break;
+                    default:
+                        rezultatCenovnik = cenovnik[i][0];
+                        console.log(rezultatCenovnik);
+                }
+            }
+        }
+    }
+    else {
+        // console.log('NE PRIPADA opciji za matricu cenovnika');
+        let rezultatCenovnik = parseFloat(padajuci[0].value);
+    }
+
+    // if (tiraz.value > 10) {
+    //     rezultatCenovnik = parseFloat(padajuci[0].value) * 0.9;
+    // }
+    // if (tiraz.value > 50) {
+    //     rezultatCenovnik = parseFloat(padajuci[0].value) * 0.8;
+    // }
+    // if (tiraz.value > 100) {
+    //     rezultatCenovnik = parseFloat(padajuci[0].value) * 0.7;
+    // }
+    // if (tiraz.value > 250) {
+    //     rezultatCenovnik = parseFloat(padajuci[0].value) * 0.6;
+    // }
+    // if (tiraz.value > 500) {
+    //     rezultatCenovnik = parseFloat(padajuci[0].value) * 0.5;
+    // }
+
+    for (let i = 1; i < padajuci.length; i++) {
+        rezultat += parseFloat(padajuci[i].value);
+    }
+    if (!isNaN(rezultat) && rezultat > 0 && padajuci[0].value !== "0") {
+        prikaziCenu.value = (rezultat + rezultatCenovnik) * tiraz.value;
+        prikaziCenu.style.color = '#495057';
+        prikaziCenu.style.fontWeight = 'bold';
+    }
+    else {
+        prikaziCenu.value = 'Odaberite jos opcija kako bi proracun bio tacan...';
+        prikaziCenu.style.fontWeight = 'normal';
+        prikaziCenu.style.color = 'red';
+    }
+    // prikaziCenu.style.border = "1px solid #ced4da";
+    // console.log(rezultat);
+}
